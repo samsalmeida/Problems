eeee042c8de8d61fc857129931a79f67bf0e7db5
samyravitoria <39337602+samyravitoria@users.noreply.github.com>
2018-06-02 20:07:53 -0300

    Create garden.cpp

---
+++ Codeforces/garden.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using  namespace std;
+
+const int MAXN=110;
+
+int n, k;
+int a[MAXN];
+
+int main(){
+	scanf("%d %d", &n, &k);
+
+	for(int i=0;i<n;++i) scanf("%d", &a[i]);
+
+	int r=0x3f3f3f3f;
+
+	for(int i=0;i<n;++i){
+		if(k%a[i]==0){
+			r=min(r, k/a[i]);
+		}
+	}
+
+	printf("%d\n", r);
+	return 0;
+}
