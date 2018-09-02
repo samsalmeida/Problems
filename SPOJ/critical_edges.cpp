d6ec90783f4e43a78213788c4ce133ee1db1c66f
Samyra Vitória <39337602+samyravitoria@users.noreply.github.com>
2018-09-01 23:15:44 -0300

    Update critical_edges.cpp

--- SPOJ/critical_edges.cpp
+++ SPOJ/critical_edges.cpp
@@ -1,86 +1,90 @@
+// Critical Edges - SPOJ
+// Complexity: O(nc*(n + m'*log m')) , m' = number of critical edges
+// By Samyra Vitória
+
 #include <bits/stdc++.h>
-#define F first
-#define S second
- 
+
 using namespace std;
- 
-typedef pair<int, int> pii;
-const int MAXN=8e+2;
 
-int nc;
-int n, m, c;
-int vis[MAXN], ft[MAXN], t[MAXN], sub[MAXN];
+const int MAXN=7e2+10;
 
+struct ed{
+	int x, y;
+
+	bool operator<(const ed& o) const{
+		if(o.x!=x){
+			return x<o.x;
+		}
+		return y<o.y;
+	}
+};
+
+int n, m, nc;
+int t, mk[MAXN], ft[MAXN], low[MAXN], s[MAXN];
 vector<int> graph[MAXN];
-vector<pii> edges;
- 
+vector<ed> bridges;
+
 void dfs(int u){
-	vis[u]=1;
-	t[u]=sub[u]=++c;
-	for(int i=0;i<graph[u].size();++i){
- 
-		int v=graph[u][i];
- 		
- 		if(!vis[v]){
- 			ft[v]=u;
- 			dfs(v);
- 			sub[u]=min(sub[u], sub[v]);
- 			if(sub[v]>t[u]){
- 				int x=u, y=v;
- 				if(x>y) swap(x, y);
- 				edges.push_back(pii(x, y));
- 			}
- 		}else if(v!=ft[u]){
- 			sub[u]=min(sub[u], t[v]);
- 		}
+	mk[u]=1;
+	low[u]=s[u]=++t;
+
+	for(auto v: graph[u]){
+
+		if(!mk[v]){
+			ft[v]=u;
+			dfs(v);
+			low[u]=min(low[u], low[v]);
+			if(low[v]>s[u]){
+			
+				if(v>u) bridges.push_back({u, v});
+				else bridges.push_back({v, u});
+			}
+		}else if(v!=ft[u]){
+			low[u]=min(low[u], s[v]);
+		}
 	}
 }
 
-bool comp(pii a, pii b){
-	if(a.F!=b.F) return a.F<b.F;
-	return a.S<b.S;
+void clear(int tam){
+	t=0;
+	memset(ft, 0, sizeof ft);
+	memset(mk, 0, sizeof mk);
+	memset(low, 0, sizeof low);
+	memset(s, 0, sizeof s);
+	bridges.clear();
+	for(int i=1;i<=tam;++i) graph[i].clear();
 }
 
 int main(){
+
 	scanf("%d", &nc);
- 
+	
 	for(int j=1;j<=nc;++j){
- 
-		scanf("%d %d", &n, &m);
- 
+		
+		scanf("%d%d", &n, &m);
+	
 		for(int i=0;i<m;++i){
 			int u, v;
-			scanf("%d %d", &u, &v);
- 
+			scanf("%d%d", &u, &v);
+	
 			graph[u].push_back(v);
 			graph[v].push_back(u);
 		}
- 
+
 		dfs(1);
-		
 		printf("Caso #%d\n", j);
-		int s=edges.size();
-		if(s!=0){
-			printf("%d\n", s);
-			sort(edges.begin(), edges.end(), comp);
-			for(int i=0;i<s;++i){
-				printf("%d %d\n", edges[i].F, edges[i].S);
+		if(bridges.size()>0){
+			printf("%d\n", bridges.size());
+			sort(bridges.begin(), bridges.end());
+
+			for(ed at: bridges){
+				printf("%d %d\n", at.x, at.y);
 			}
-		}else{
-			printf("Sin bloqueos\n");
 		}
 
-		memset(sub, 0, sizeof(sub));
-		memset(vis, 0, sizeof(vis));
-		memset(ft, 0, sizeof(ft));
-		memset(t, 0, sizeof(t));
+		else printf("Sin bloqueos\n");
 
-		for(int i=0;i<=n;++i){
-			graph[i].clear();
-		}
-		edges.clear();
-		c=0;
+		clear(n);
 	}
- 
 	return 0;
 }
