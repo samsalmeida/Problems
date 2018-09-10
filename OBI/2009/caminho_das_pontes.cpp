ea9e48104c958a28e9eb992c5028ef4818aff592
Samyra Vitória <39337602+samyravitoria@users.noreply.github.com>
2018-09-09 22:32:53 -0300

    Update caminho_das_pontes.cpp

--- OBI/2009/caminho_das_pontes.cpp
+++ OBI/2009/caminho_das_pontes.cpp
@@ -1,72 +1,77 @@
+// Pizza - Programação Nível 2 - Fase 2 - OBI 2007
+// Menor Caminho - Dijkstra 
+// Complexidade: O(m*log n)
+// By Samyra Almeida
+
 #include <bits/stdc++.h>
-    using namespace std;
+using namespace std;
      
-    typedef pair<int, int> pii;   
-    const int MAXN=1.01e+4;
+typedef pair<int, int> pii;   
+const int MAXN=1e4+10;
      
-    #define INFINITO 999999999
+#define INFINITO 999999999
      
-    int n, m, dist[MAXN], proc[MAXN];
-    vector<pii> vis[MAXN];
+int n, m, dist[MAXN], proc[MAXN];
+vector<pii> vis[MAXN];
      
-    void Dijkstra(int S){
+void Dijkstra(int S){
     	
-    	for(int i = 1;i <=( n+1);i++) dist[i] = INFINITO; 
-    	dist[S] = 0;
+    for(int i = 1;i <=(n+1);i++) dist[i] = INFINITO; 
+    dist[S] = 0;
+    
+    priority_queue< pii, vector<pii>, greater<pii> > row; 
     	
-    	priority_queue< pii, vector<pii>, greater<pii> > row; 
+    row.push( pii(dist[S], S) );
     	
-    	row.push( pii(dist[S], S) );
-    	
-    	while(true){ 
+    while(true){ 
     		
-    		int p = -1;
+    	int p = -1;
     	
-    		while(!row.empty()){
+    	while(!row.empty()){
     			
-    			int a=row.top().second;
-    			row.pop();
+    		int a=row.top().second;
+    		row.pop();
     			
-    			if(!proc[a]){ 
-    				p=a;
-    				break;
-    			}
-    			
+    		if(!proc[a]){ 
+    			p=a;
+    			break;
     		}
-    		
-    		if(p==-1) break; 
-    		
-    		proc[p]=true;
-    		
-    		for(int i=0;i<(int)vis[p].size();++i){
     			
-    			int d=vis[p][i].first;
-    			int a=vis[p][i].second;
+    	}
+    		
+    	if(p==-1) break; 
+    		
+    	proc[p]=true;
+    		
+    	for(int i=0;i<(int)vis[p].size();++i){
+    			
+    		int d=vis[p][i].first;
+    		int a=vis[p][i].second;
     			
     			
-    			if(dist[a]>dist[p]+d){  
+    		if(dist[a]>dist[p]+d){  
      
-    				dist[a]=dist[p]+d;   
-    				row.push(pii(dist[a], a));
-    			}
+    			dist[a]=dist[p]+d;   
+    			row.push(pii(dist[a], a));
     		}
     	}
     }
+}
      
-    int main(){
+int main(){
     	
-    	scanf("%d %d", &n, &m);
-    	for(int i = 1;i <= m;i++){
+    scanf("%d %d", &n, &m);
+    for(int i = 1;i <= m;i++){
     		
-    		int x, y, t;
-    		scanf("%d %d %d", &x, &y, &t);
+    	int x, y, t;
+    	scanf("%d %d %d", &x, &y, &t);
      
-    		vis[x].push_back(pii(t, y));
-    		vis[y].push_back(pii(t, x));
-    	}
-    	
-    	Dijkstra(0);
-    	cout << dist[n+1] << "\n"; 
-    	
-    	return 0;
+    	vis[x].push_back(pii(t, y));
+    	vis[y].push_back(pii(t, x));
     }
+    	
+    Dijkstra(0);
+    cout << dist[n+1] << "\n"; 
+    	
+    return 0;
+} 
